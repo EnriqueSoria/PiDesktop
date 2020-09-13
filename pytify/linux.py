9b69bdbdbb21a8a3695a45de736ef1d15d42d392
EnriqueSoria <ensomag@inf.upv.es>
2020-09-13 16:54:31 +0200

    Update

--- pytify/linux.py
+++ pytify/linux.py
@@ -3,14 +3,36 @@ from __future__ import absolute_import, unicode_literals
 from pytify.dbus.metadata import Metadata
 from pytify.dbus.interface import Interface
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CustomMetadata(Metadata):
+    def get_current_playing(self):
+        playing = {}
+
+        for key, value in self.get_metadata().items():
+            if key == 'xesam:album':
+                playing['album'] = value
+
+            elif key == 'xesam:title':
+                playing['title'] = value
+
+            elif key == 'xesam:artist':
+                playing['artist'] = value[0]
+
+        return f"{playing['title']} - {playing['artist']}"
+
 
 class Linux:
     def __init__(self):
         self.interface = Interface.factory('org.mpris.MediaPlayer2.Player')
 
-        self.metadata = Metadata()
+        self.metadata = CustomMetadata()
 
     def next(self):
+        print("next song")
         self.interface.Next()
 
     def prev(self):
--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
-MQTT_SERVER = '192.168.43.174'
+
+import os
+
+from decouple import AutoConfig
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+config = AutoConfig(f'{BASE_DIR}/')
+
+MQTT_SERVER = config("MQTT_SERVER", default="localhost")
